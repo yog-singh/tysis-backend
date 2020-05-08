914b860e78abc8a20510acd4ce1d1ce71fa35300
Yogesh Singh <yogesh.singh.ams5@gmail.com>
2020-05-08 11:53:10 +0530

    sixth

--- public/script/pyts.py
+++ public/script/pyts.py
@@ -1,3 +1,9 @@
-import sys
-print("File Recieved: ",sys.argv[1])
-sys.stdout.flush()
+import os
+from flask import Flask, flash, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/upload', methods=['POST'])
+def basic():
+    if request.method == 'POST':
+        return 'Hello World'
